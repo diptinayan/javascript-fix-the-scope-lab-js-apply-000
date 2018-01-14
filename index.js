d0558bca82cf33624a050caf569341290171bd65
Dipti Nayan <diptinayan21@gmail.com>
2018-01-14 04:41:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ function yourAnimal() {
 
 function add2(n) {
   n=777
+  return n
 }
-return n
+
   // Feel free to move things around!
 
 
-}
 
 var funkyFunction = function() {
   return function() {
